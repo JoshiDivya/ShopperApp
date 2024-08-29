442b727b80dfec90f61ed12ccc3951a5533c4381
Divya Joshi <divyajoshics09@gmail.com>
2024-08-29 21:36:27 +0300

    Update CategoryComponent.jsx

--- src/components/MainContent/CategoryComponent/CategoryComponent.jsx
+++ src/components/MainContent/CategoryComponent/CategoryComponent.jsx
@@ -9,8 +9,8 @@ import { getCategoryList } from "../../app/fetchAPI";
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
-      'X-RapidAPI-Key': '7be3eeea50msh8ef01ec06ced9dap195957jsn73ca7f2ac984'
+      'X-RapidAPI-Host': Process.env.RAPIDAPI_HOST
+       'X-RapidAPI-Key': Process.env.RAPIDAPI_KEY
     }
   };
 
